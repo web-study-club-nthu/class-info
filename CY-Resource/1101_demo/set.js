1ec5be715a3f5ab37e435978501ea8961620e408
chunyou0830 <chunyou0830@gmail.com>
2015-11-29 17:43:45 +0800

    1129 Slide upload

--- CY-Resource/1101_demo/set.js
+++ CY-Resource/1101_demo/set.js
@@ -1,7 +1,12 @@
-$(document).ready(function (){
-	$('.contain').blurjs({
-		source: 'body',
-		radius: 7,
-		overlay: 'rgba(255,255,255,0.4)'
-	});
+$(document).ready(function() {
+var movementStrength = 25;
+var height = movementStrength / $(window).height();
+var width = movementStrength / $(window).width();
+$("body").mousemove(function(e){
+          var pageX = e.pageX - ($(window).width() / 2);
+          var pageY = e.pageY - ($(window).height() / 2);
+          var newvalueX = width * pageX * -2 - 25;
+          var newvalueY = height * pageY * -0 - 50;
+          $('#bg-img').css("background-position", newvalueX+"px     "+newvalueY+"px");
+});
 });
---
+++ CY-Resource/1129_slide/pageset.js
@@ -0,0 +1,33 @@
+// Timer --------------------
+
+var end = new Date('11/29/2015 9:00 PM');
+
+    var _second = 1000;
+    var _minute = _second * 60;
+    var _hour = _minute * 60;
+    var _day = _hour * 24;
+    var timer;
+
+    function showRemaining() {
+        var now = new Date();
+        var distance = end - now;
+        if (distance < 0) {
+
+            clearInterval(timer);
+            document.getElementById('countdown').innerHTML = 'Time up!';
+
+            return;
+        }
+        var days = Math.floor(distance / _day);
+        var hours = Math.floor((distance % _day) / _hour);
+        var minutes = Math.floor((distance % _hour) / _minute);
+        var seconds = Math.floor((distance % _minute) / _second);
+
+        //document.getElementById('countdown').innerHTML = '時間還剩下 ';
+        //document.getElementById('countdown').innerHTML += days + '天 ';
+        document.getElementById('countdown').innerHTML = hours + ' Hr ';
+        document.getElementById('countdown').innerHTML += minutes + ' Min ';
+        document.getElementById('countdown').innerHTML += seconds + ' Sec';
+    }
+
+    timer = setInterval(showRemaining, 1000);
